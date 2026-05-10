eb32b705d93e5b0fb9e0ab2811aa4a7bd901a3a0
Osmeeta Chouhan <osmee08@gmail.com>
2026-05-10 06:16:18 +0530

    Add files via upload

---
+++ value.c
@@ -0,0 +1,12 @@
+// 3.0 + 1 will be:
+// a. Integer.
+// b. Floating point number.
+// c. Character.
+
+#include <stdio.h>
+
+int main(){
+    float a = 3.0 + 1 ;
+    printf("The value of a is %f", a);
+    return 0;
+}
